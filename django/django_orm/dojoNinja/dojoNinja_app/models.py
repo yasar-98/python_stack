8ba65ad1796ebb2195ff564e7805ff0049b37684
yasar-98 <yasar_98@outlook.sa>
2021-05-23 15:09:53 +0300

    dojo and ninja by shell

---
+++ django/django_orm/dojoNinja/dojoNinja_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    desc= models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Ninja(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dojo = models.ForeignKey(Dojo, related_name="ninjas", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
